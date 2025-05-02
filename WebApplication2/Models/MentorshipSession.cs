c0af271bda6e3920be407dd9c0b348a9774dc3cb
nithishkumarbnk <nithishkumar.bhu07@gmail.com>
2025-05-02 07:41:01 +0530

    Moved files into myfolder

---
+++ WebApplication2/Models/MentorshipSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using WebApplication2.Models;
+
+namespace WebApplication2.Models
+{
+
+
+    public class MentorshipSession
+    {
+        public int MentorshipSessionId { get; set; }  // Primary Key
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+
+        // Foreign key for Student
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+
+        // Foreign key for Mentor
+        public int MentorId { get; set; }
+        public virtual Mentor Mentor { get; set; }
+    }
+
+
+}
