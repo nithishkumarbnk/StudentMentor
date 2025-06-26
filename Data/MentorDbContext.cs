6f95ca9f2a0133a4debff7f6c51e1f1f2cc82202
nithishkumarbnk <nithishkumar.bhu07@gmail.com>
2025-06-26 08:38:46 +0530

    Initial commit of .NET Student Mentor app

---
+++ Data/MentorDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+using WebApplication2.Models;
+
+namespace WebApplication2.Models
+{
+    public class MentorDb : DbContext
+    {
+        public MentorDb() : base("DefaultConnection") // Make sure this matches your connection string name
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Mentor> Mentors { get; set; }
+        public DbSet<MentorshipSession> MentorshipSessions { get; set; }
+    }
+}
---
+++ Data/WebApplication2Context.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class WebApplication2Context : DbContext
+    {
+        public WebApplication2Context(DbContextOptions<WebApplication2Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Mentor> Mentor{ get; set; }
+        public DbSet<MentorshipSession> MentorshipSession { get; set; }
+    }
+}
